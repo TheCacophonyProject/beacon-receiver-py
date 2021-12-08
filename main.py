0d01ccb8b84fb3bb22f8fd2fbb60a4254e1c81eb
gferraro <g.ferraro22@gmail.com>
2021-12-09 12:20:56 +1300

    updated to print out classifications

--- main.py
+++ main.py
@@ -9,49 +9,78 @@ import machine
 devices = {}
 pin = machine.Pin(2, machine.Pin.OUT)
 
-dev={
-    "scanning":False,
-    "last_received_time": 0
-    }
+recording_type = 2
+classification_type = 3
+
+labels = {
+    0: "bird",
+    1: "cat",
+    2: "false-positive",
+    3: "hedgehog",
+    4: "human",
+    5: "leporidae",
+    6: "mustelid",
+    7: "possum",
+    8: "rodent",
+    9: "vehicle",
+    10: "wallaby",
+}
+
+dev = {"scanning": False, "last_received_time": 0}
+
 
 def bt_irq(event, data):
     if event == 5:
         addr_type, addr, adv_type, rssi, adv_data = data
         # Check that data is long enough
         l = adv_data[0]
-        if l < 9 or l != len(adv_data)-1:
+        if l < 9 or l != len(adv_data) - 1:
             return
         ad_type = adv_data[1]
-        man_id = int.from_bytes(adv_data[2:4], 'big')
+        man_id = int.from_bytes(adv_data[2:4], "big")
         version = adv_data[4]
-        # Check that data is from one of our cameras and a version that 
+        # Check that data is from one of our cameras and a version that
         if ad_type != 0xFF or man_id != 0x1212 or version != 0x01:
             return
-        #print("======")
-        #print(ubinascii.hexlify(adv_data))
-        
+        # print("======")
+        # print(ubinascii.hexlify(adv_data))
+
         # Check the CRC of the data
-        crc = int.from_bytes(adv_data[-4:], 'big')
+        crc = int.from_bytes(adv_data[-4:], "big")
         crc_data = ubinascii.crc32(adv_data[4:-4])
         if crc != crc_data:
             print("CRC did not match")
-            
-        device_id = int.from_bytes(adv_data[5:7], 'big')
+
+        device_id = int.from_bytes(adv_data[5:7], "big")
         data_type = adv_data[7]
         data = adv_data[8:-4]
-        
+
         process_data(device_id, data_type, data)
-        
+
     elif event == 6:
         dev["scanning"] = False
 
+
 def process_data(device_id, data_type, data):
-    #TODO
     print("New beacon call!")
     print("Device ID:", device_id)
-    print("Data type:", data_type)
-    print("Data:", ubinascii.hexlify(data))
     dev["last_received_time"] = time.ticks_ms()
+    if data_type == recording_type:
+        print("Recording started")
+    elif data_type == classification_type:
+        handle_classification(data)
+
+
+
+
+def handle_classification(classifications):
+    top_pick = labels[int(classifications[1])]
+    top_confidence = int(classifications[2])
+    second_pick = labels[int(classifications[3])]
+    second_confidence = int(classifications[4])
+    print("1:", top_pick, " ", top_confidence, "%")
+    print("2:", second_pick, " ", second_confidence, "%")
+
 
 print("starting")
 ble = bluetooth.BLE()
@@ -61,5 +90,5 @@ while True:
     dev["scanning"] = True
     ble.gap_scan(3000, 30000, 30000)
     while dev["scanning"]:
-        pin.value(dev["last_received_time"] > time.ticks_ms()- 5000)
+        pin.value(dev["last_received_time"] > time.ticks_ms() - 5000)
         time.sleep(0.2)
